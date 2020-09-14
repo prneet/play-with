decc55cd4242f2d4628fc65804c4b11c74503086
prneet <69181346+prneet@users.noreply.github.com>
2020-09-14 10:12:14 +0900

    ビュー編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'recruits#index'
   resources :recruits
+  get 'recruit_with_pass/:id', to: 'recruits#recruit_with_pass'
+  post 'recruit_with_pass/:id', to: 'recruits#authenticate'
 end
