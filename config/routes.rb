42d21dae89db3d68652d42c05728f771b883fe15
prneet <69181346+prneet@users.noreply.github.com>
2020-09-16 19:45:30 +0900

    一覧ページフロント実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'recruits#index'
-  resources :recruits
+  resources :recruits do
+    collection do
+      get 'search'
+    end
+  end
   get 'recruit_with_pass/:id', to: 'recruits#recruit_with_pass'
   post 'recruit_with_pass/:id', to: 'recruits#authenticate'
 end
